22e73c8e9ba4295631b06cdf562cac70a2f84343
Mark Jervelund <mark@jervelund.com>
2017-09-28 11:23:03 +0200

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/benchmarkblocked.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/benchmarkblocked.dir/src/benchmark.cpp.o"
+  "CMakeFiles/benchmarkblocked.dir/src/dgemm-blocked.cpp.o"
+  "benchmarkblocked.pdb"
+  "benchmarkblocked"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/benchmarkblocked.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/benchmarknaive.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/benchmarknaive.dir/src/benchmark.cpp.o"
+  "CMakeFiles/benchmarknaive.dir/src/dgemm-naive.cpp.o"
+  "benchmarknaive.pdb"
+  "benchmarknaive"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/benchmarknaive.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
