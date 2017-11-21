86a4c24a92f54d907c5653fc0675f24815f75f4c
Mark Jervelund <mark@jervelund.com>
2017-11-21 16:40:13 +0100

    reduced benchmark to 10 threads.

--- mandatory2/testcode/mpi.sh
+++ mandatory2/testcode/mpi.sh
@@ -1,13 +1,13 @@
 rm mpi.txt
 
-mpirun -n 40 -hostfile imada-hostfile.alive mand2_mpi_opti -n 500 -no -s mpi.txt
-mpirun -n 40 -hostfile imada-hostfile.alive mand2_mpi_opti -n 1000 -no -s mpi.txt
-mpirun -n 40 -hostfile imada-hostfile.alive mand2_mpi_opti -n 2000 -no -s mpi.txt
-mpirun -n 40 -hostfile imada-hostfile.alive mand2_mpi_opti -n 4000 -no -s openmp.txt
-mpirun -n 40 -hostfile imada-hostfile.alive mand2_mpi_opti -n 8000 -no -s mpi.txt
-mpirun -n 40 -hostfile imada-hostfile.alive mand2_mpi_opti -n 16000 -no -s mpi.txt
-mpirun -n 40 -hostfile imada-hostfile.alive mand2_mpi_opti -n 32000 -no -s mpi.txt
-mpirun -n 40 -hostfile imada-hostfile.alive mand2_mpi_opti -n 64000 -no -s mpi.txt
-mpirun -n 40 -hostfile imada-hostfile.alive mand2_mpi_opti -n 128000 -no -s mpi.txt
-mpirun -n 40 -hostfile imada-hostfile.alive mand2_mpi_opti -n 256000 -no -s mpi.txt
+mpirun -n 10 -hostfile imada-hostfile.alive mand2_mpi_opti -n 500 -no -s mpi.txt
+mpirun -n 10 -hostfile imada-hostfile.alive mand2_mpi_opti -n 1000 -no -s mpi.txt
+mpirun -n 10 -hostfile imada-hostfile.alive mand2_mpi_opti -n 2000 -no -s mpi.txt
+mpirun -n 10 -hostfile imada-hostfile.alive mand2_mpi_opti -n 4000 -no -s openmp.txt
+mpirun -n 10 -hostfile imada-hostfile.alive mand2_mpi_opti -n 8000 -no -s mpi.txt
+mpirun -n 10 -hostfile imada-hostfile.alive mand2_mpi_opti -n 16000 -no -s mpi.txt
+mpirun -n 10 -hostfile imada-hostfile.alive mand2_mpi_opti -n 32000 -no -s mpi.txt
+mpirun -n 10 -hostfile imada-hostfile.alive mand2_mpi_opti -n 64000 -no -s mpi.txt
+mpirun -n 10 -hostfile imada-hostfile.alive mand2_mpi_opti -n 128000 -no -s mpi.txt
+mpirun -n 10 -hostfile imada-hostfile.alive mand2_mpi_opti -n 256000 -no -s mpi.txt
 ./autograder -v mpi -s mpi.txt
