7f23865075d2c2ccc4817fb6c951d56dc67ece35
Mark Jervelund <mark@jervelund.com>
2017-11-21 16:36:44 +0100

    updated the benchmark script

--- mandatory2/testcode/mpi.sh
+++ mandatory2/testcode/mpi.sh
@@ -1,12 +1,13 @@
 rm mpi.txt
-./mpi -n 500 -no -s mpi.txt
-./mpi -n 1000 -no -s mpi.txt
-./mpi -n 2000 -no -s mpi.txt
-./mpi -n 4000 -no -s openmp.txt
-./mpi -n 8000 -no -s mpi.txt
-./mpi -n 16000 -no -s mpi.txt
-./mpi -n 32000 -no -s mpi.txt
-./mpi -n 64000 -no -s mpi.txt
-./mpi -n 128000 -no -s mpi.txt
-./mpi -n 256000 -no -s mpi.txt
+
+mpirun -n 40 -hostfile imada-hostfile.alive mand2_mpi_opti -n 500 -no -s mpi.txt
+mpirun -n 40 -hostfile imada-hostfile.alive mand2_mpi_opti -n 1000 -no -s mpi.txt
+mpirun -n 40 -hostfile imada-hostfile.alive mand2_mpi_opti -n 2000 -no -s mpi.txt
+mpirun -n 40 -hostfile imada-hostfile.alive mand2_mpi_opti -n 4000 -no -s openmp.txt
+mpirun -n 40 -hostfile imada-hostfile.alive mand2_mpi_opti -n 8000 -no -s mpi.txt
+mpirun -n 40 -hostfile imada-hostfile.alive mand2_mpi_opti -n 16000 -no -s mpi.txt
+mpirun -n 40 -hostfile imada-hostfile.alive mand2_mpi_opti -n 32000 -no -s mpi.txt
+mpirun -n 40 -hostfile imada-hostfile.alive mand2_mpi_opti -n 64000 -no -s mpi.txt
+mpirun -n 40 -hostfile imada-hostfile.alive mand2_mpi_opti -n 128000 -no -s mpi.txt
+mpirun -n 40 -hostfile imada-hostfile.alive mand2_mpi_opti -n 256000 -no -s mpi.txt
 ./autograder -v mpi -s mpi.txt
